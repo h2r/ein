eb16a5e5a44df4b755bc3995f1a2256d2197d1b1
oberlin <oberlin@cs.brown.edu>
2014-11-01 18:25:55 -0400

    pilot_baxter added.

---
+++ util/dave_auto_pilot.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+rosrun baxter_tools camera_control.py -c left_hand_camera
+rosrun baxter_tools camera_control.py -c right_hand_camera
+#rosrun baxter_tools camera_control.py -c head_camera
+
+rosrun baxter_tools camera_control.py -o right_hand_camera -r 640x400
+rosrun baxter_tools camera_control.py -o left_hand_camera -r 640x400
+
+array=( "capture_object" "publish_detections" "capture_annotated_objects" "capture_hard_class" )
+for i in {0..3}
+do
+  echo ${array[i]}
+  rosparam set /${array[i]}/gray_box_left 150
+  rosparam set /${array[i]}/gray_box_right 150
+  rosparam set /${array[i]}/gray_box_top 10
+  rosparam set /${array[i]}/gray_box_bot 60
+  rosparam set /${array[i]}/threshold_fraction .5
+  rosparam set /${array[i]}/density_decay .3
+done
+
+
