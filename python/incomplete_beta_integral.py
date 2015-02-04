113670e0fb95e790d7c425c534e61d3b9bea0041
Stefanie Tellex <stefie10@cs.brown.edu>
2015-02-04 14:04:42 -0500

    plotting incomplete beta to get intuition
    Former-commit-id: 1cc5a68ca4da2041b1a4f81b482792a49d4f5118

--- python/incomplete_beta_integral.py
+++ python/incomplete_beta_integral.py
@@ -1,24 +1,37 @@
-from scipy.special import betainc, beta
+from scipy.special import betainc
 import matplotlib.pyplot as plt; plt.rc('text', usetex=True)
 import pylab as mpl
 import numpy as na
 
-def regularized_incomplete_beta(a, b, x):
-    return betainc(a, b, x) * beta(a, b)
+def plot(successes, failures):
+    a = successes + 1
+    b = failures + 1
 
-def plot(S, F):
-    a = S + 1
-    b = F + 1
+    X = na.arange(0, 1, 0.01)
+    Y = [betainc(a, b, x) for x in X]
+    mpl.plot(X, Y, label="successes=%d; failures=%d" % (successes, failures))
 
-    X = na.arange(0, 1, 0.1)
-    Y = [regularized_incomplete_beta(a, b, x) for x in X]
-    mpl.plot(X, Y, label="S=%d; F=%d" % (S, F))
+    if (successes + failures == 0):
+        mu = 0.5
+    else:
+        mu = float(successes) / (successes + failures)
+        
+    muX = [mu]
+    muY = [0.5]
+    mpl.scatter(muX, muY, marker="o", color="k")  # plot mean given successes and failures
     mpl.ylabel("$\Pr(\mu <= x)$")
     mpl.xlabel("$x$")
-    mpl.legend()
+    mpl.legend(loc="lower right")
 
 def main():
+    """
+    Smoke test for incomplete beta.
+    """
     plot(0, 0)
+    plot(5, 5)
+    plot(5, 10)
+    plot(10, 5)
+    mpl.title("Value of the incomplete beta function for different values of $x$")
     mpl.show()
 
 if __name__ == "__main__":
