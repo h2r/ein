369a9232b02acd47f9ecb016d3f2ceb9c7aa8023
Stefanie Tellex <stefie10@cs.brown.edu>
2015-02-04 00:03:45 -0500

    plotting regret with thompson sampling, starting to write algorithm B
    Former-commit-id: 56c38c7b504a2c563cbc656facea337c88ad82b2

--- python/newbandit.py
+++ python/newbandit.py
@@ -1,6 +1,7 @@
 import numpy as na
 import random
 import scipy.stats
+import pylab as mpl
 
 class Bandit(object):
 
@@ -13,34 +14,29 @@ class Bandit(object):
         self.narms = len(arms)
         self.actions = na.array(list(range(self.narms)))
         self.best_ai = na.argmax(self.arms)
-        
+        self.log = []
 
     def sample(self, armnum):
-        if random.random() < self.arms[armnum]:
-            return 1
-        else:
-            return -1
 
-    def expectedReturn(self, actionDistribution):
+        if random.random() < self.arms[armnum]:
+            result = 1
+        else:
+            result = -1
+        self.log.append((armnum, result))
+        return result
+
+    def reset_log(self):
+        self.log = []
+
+
+
+    def expected_return(self, actionDistribution):
         assert len(actionDistribution) == self.narms
         return na.dot(actionDistribution, self.arms)
 
     def regret(self, actionDistribution):
-        return self.arms[self.best_ai] - self.expectedReturn(actionDistribution)
+        return self.arms[self.best_ai] - self.expected_return(actionDistribution)
 
-class LoggedBandit(object):
-    def __init__(self, narms):
-        self.bandit = Bandit(narms)
-        self.log = []
-
-    def reset(self):
-        self.log = []
-
-    def sample(self, armnum):
-        result = self.bandit.sample(armnum)
-        self.log.append(armnum, result)
-    def regret(self, actionDistribution):
-        return self.bandit.regret(actionDistribution)
 
 class Policy(object):
     
@@ -49,6 +45,33 @@ class Policy(object):
 
     def selectAction(self, bandit):
         raise NotImplementedError()
+
+
+class AlgorithmB(Policy):
+    def __init__(self):
+        pass
+
+    def train(self, bandit, max_budget):
+        self.S = na.zeros(bandit.narms)
+        self.F = na.zeros(bandit.narms)
+
+        for iterations in range(max_budget):
+            sampled_params = []
+            for a_i in bandit.actions:
+                p = scipy.stats.beta.rvs(self.S[a_i] + 1, self.F[a_i] + 1)
+                sampled_params.append(p)
+
+            best_ai = na.argmax(sampled_params)
+
+            r = bandit.sample(best_ai)
+
+            if r == 1:
+                self.S[best_ai] += 1
+            else:
+                self.F[best_ai] += 1
+
+        self.marginals = [(s + 1)/(s + f + 2) for (s, f) in zip(self.S, self.F)]
+        
 
 class ThompsonSampling(Policy):
     def __init__(self):
@@ -81,20 +104,40 @@ class ThompsonSampling(Policy):
         delta = na.zeros(len(self.S))
         delta[self.bestAction()] = 1
         return delta
+    def __str__(self):
+        return "Thompson Sampling"
         
 
 def main():
-    bandit = Bandit([0.2, 0.3, 0.7])
+    bandit = Bandit([0.1, 0.1, 0.9, 0.1, 0.9, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
 
     print bandit.arms
 
     thompson_sampling = ThompsonSampling()
     for method in [thompson_sampling]:
-        method.train(bandit, 3)
+        results = []
+        for budget in na.arange(0, 110, 10):
+            regrets = []
+            budgets = []
+            for iteration in range(50):
+                bandit.reset_log()
+                method.train(bandit, budget)
+                budgets.append(len(bandit.log))
+                regret = bandit.regret(method.actionDistribution())
+                regrets.append(regret)
+
+            results.append((budgets, regrets))
         
-        regret = bandit.regret(method.actionDistribution())
-        print regret
-        
+        X = [na.mean(b) for (b, r) in results]
+        Y = [na.mean(r) for (b, r) in results]
+        xerr = [scipy.stats.sem(b) * 1.96 for b, r in results]
+        yerr = [scipy.stats.sem(r) * 1.96 for b, r in results]
+        mpl.errorbar(X, Y, label=str(method), xerr=xerr, yerr=yerr)
+    mpl.ylabel("Simple Regret")
+    mpl.xlabel("Training Trials")
+    mpl.axis((0, 105, -0.1, 1))
+    mpl.legend()
+    mpl.show()
 
 
 if __name__ == "__main__":
