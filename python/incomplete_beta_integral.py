0ecfbb53739ef2c95fa53c83d479eb78bb55ca3e
Stefanie Tellex <stefie10@cs.brown.edu>
2015-02-04 13:54:04 -0500

    incomplete beta
    Former-commit-id: d3e0e3a26d4a689136b1a10cdd2947f44bd62ef9

--- python/incomplete_beta_integral.py
+++ python/incomplete_beta_integral.py
@@ -1,15 +1,20 @@
 from scipy.special import betainc, beta
+import matplotlib.pyplot as plt; plt.rc('text', usetex=True)
 import pylab as mpl
 import numpy as na
 
+def regularized_incomplete_beta(a, b, x):
+    return betainc(a, b, x) * beta(a, b)
 
 def plot(S, F):
     a = S + 1
     b = F + 1
 
     X = na.arange(0, 1, 0.1)
-    Y = [betainc(a, b, x) * beta(a, b) for x in X]
+    Y = [regularized_incomplete_beta(a, b, x) for x in X]
     mpl.plot(X, Y, label="S=%d; F=%d" % (S, F))
+    mpl.ylabel("$\Pr(\mu <= x)$")
+    mpl.xlabel("$x$")
     mpl.legend()
 
 def main():
