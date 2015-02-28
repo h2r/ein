3965e6d5dfb61dd2707669678c8ab8d2929a7fbf
Stefanie <stefie10@cs.brown.edu>
2015-02-27 21:53:18 -0500

    infrastructure to quickly create new words
    Former-commit-id: 9a7d9b4739c75b64e4e1a5d1e02fef4f7f374eb9

--- src/ein_words.cpp
+++ src/ein_words.cpp
@@ -1,58 +1,155 @@
-class PauseWord: public Word 
+
+// Preprocessor macro to automatically create class structure for words
+// extracted from the case statement of evil (despair)
+// WORD(gName)
+// // code here
+// CODE(123)
+
+#define WORD(gName) \
+class gName: public Word \
+{ \
+public: \
+  virtual string name() { \
+    string str = #gName; \
+    str[0] = tolower(str[0]); \
+    return str; \
+  } \
+  virtual void execute() 
+
+#define CODE(code) \
+  virtual int character_code() { \
+    return code; \
+  } 
+
+#define END_WORD };
+
+
+
+namespace ein_words
 {
-public:
-  virtual void execute() {
-    if (auto_pilot || (holding_pattern != 0)) {
-      pilot_call_stack.push_back('C');
-    } else {
-      holding_pattern = 0;
-      auto_pilot = 0;
-      go_on_lock = 0;
+
+WORD(PrintState)
+{
+  cout << endl;
+  cout << "Current EE Position (x,y,z): " << currentEEPose.px << " " << currentEEPose.py << " " << currentEEPose.pz << endl;
+  cout << "Current EE Orientation (x,y,z,w): " << currentEEPose.qx << " " << currentEEPose.qy << " " << currentEEPose.qz << " " << currentEEPose.qw << endl;
+  cout << "True EE Position (x,y,z): " << trueEEPose.position.x << " " << trueEEPose.position.y << " " << trueEEPose.position.z << endl;
+  cout << "True EE Orientation (x,y,z,w): " << trueEEPose.orientation.x << " " << trueEEPose.orientation.y << " " << trueEEPose.orientation.z << " " << trueEEPose.orientation.w << endl;
+  cout <<
+    "eePose = {.px = " << trueEEPose.position.x << ", .py = " << trueEEPose.position.y << ", .pz = " << trueEEPose.position.z << "," << endl <<
+    "		      .ox = 0, .oy = 0, .oz = 0," << endl <<
+    "		      .qx = " << trueEEPose.orientation.x << ", .qy = " << trueEEPose.orientation.y << ", .qz = " << trueEEPose.orientation.z << ", .qw = " << trueEEPose.orientation.w << "};" << endl;
+  cout << "mostRecentUntabledZ: " << mostRecentUntabledZ << endl;
+  cout << "currentPickMode: " << pickModeToString(currentPickMode) << endl;
+  cout << "currentBoundingBoxMode: " << pickModeToString(currentBoundingBoxMode) << endl;
+  cout << "gradientServoTakeClosest: " << gradientTakeClosest << endl;
+  cout << "synchronicTakeClosest: " << synchronicTakeClosest << endl;
+  cout << "focusedClass: " << focusedClass;
+  if (focusedClass != -1) {
+    cout << " " << classLabels[focusedClass];
+  }
+  cout << endl;
+  
+  cout << "targetClass: " << targetClass;
+  if (targetClass != -1) {
+    cout << " " << classLabels[targetClass];
+  }
+  
+  cout << endl;
+  cout << endl;
+}
+  CODE('u')
+  END_WORD
+
+WORD(IncrementTargetClass)
+{
+    cout << "targetClass++ " << endl;
+    if (numClasses > 0) {
+      int newTargetClass = (targetClass + 1) % numClasses;
+      changeTargetClass(newTargetClass);
     }
-  }
-  virtual string name() {
-    return "pause";
-  }
-  virtual int character_code() {
-    return 'C';
-  }
-};
+}
+// capslock + pageup
+CODE(196437)
+END_WORD
 
-class ZUpWord: public Word
+WORD(GradientServoTakeClosest)
 {
-public:
-  virtual void execute() {
-    currentEEPose.pz += bDelta;
-  }
-  virtual string name() {
-    return "zup";
-  }
+    gradientTakeClosest = 1;
+    cout << "gradientTakeClosest = " << gradientTakeClosest << endl;
+}
+// capslock + numlock + h
+CODE(1179720)
+END_WORD
 
-  virtual int character_code() {
-    return 'w';
-  }
-};
-
-class ZDownWord: public Word
+WORD(SynchronicServoTakeClosest)
 {
-public:
-  virtual void execute() {
+    synchronicTakeClosest = 1;
+    cout << "synchronicTakeClosest = 1" << endl;
+    synServoLockFrames = 0;
+}
+// capslock + C
+CODE(196707)
+END_WORD
+
+WORD(GraspGear1)
+{
+  int thisGraspGear = 0;
+  
+  //   set drX
+  drX = ggX[thisGraspGear];
+  drY = ggY[thisGraspGear];
+  
+  //   rotate
+  setGGRotation(thisGraspGear);
+  
+  //   set currentGraspGear;
+  currentGraspGear = thisGraspGear;
+  // numlock + 1
+}
+CODE(1048625)
+END_WORD
+
+WORD(Pause)
+{
+  if (auto_pilot || (holding_pattern != 0)) {
+    pilot_call_stack.push_back('C');
+  } else {
+    holding_pattern = 0;
+    auto_pilot = 0;
+    go_on_lock = 0;
+  }
+}
+CODE('C')
+END_WORD
+
+WORD(ZUp)
+{
+  currentEEPose.pz += bDelta;
+}
+CODE('w')
+END_WORD
+
+WORD(ZDown)
+{
     currentEEPose.pz -= bDelta;
-  }
-  virtual string name() {
-    return "zdown";
-  }
+}
+CODE('s')
+END_WORD
 
-  virtual int character_code() {
-    return 's';
-  }
-};
+}
 
+using namespace  ein_words;
 std::vector<Word *> create_words() {
   std::vector<Word *> words;
-  words.push_back(new ZUpWord());
-  words.push_back(new ZDownWord());
-  words.push_back(new PauseWord());
+  words.push_back(new ZUp());
+  words.push_back(new ZDown());
+  words.push_back(new Pause());
+  words.push_back(new GraspGear1());
+  words.push_back(new SynchronicServoTakeClosest());
+  words.push_back(new GradientServoTakeClosest());
+  words.push_back(new IncrementTargetClass());
+  words.push_back(new PrintState());
   return words;
 }
 
@@ -71,5 +168,6 @@ std::map<string, Word *> create_name_to_word(std::vector<Word *> words) {
   }
   return name_to_word;
 }
+
 
 
