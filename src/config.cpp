ebc27587be6d9d8d1a436bbd65ac9abe17138e42
Stefanie Tellex <stefie10@cs.brown.edu>
2015-06-13 12:21:47 -0400

    state printing function

--- src/config.cpp
+++ src/config.cpp
@@ -1,1 +1,21 @@
 #include "config.h"
+
+
+string pickModeToString(pickMode mode) {
+  string result;
+  if (mode == STATIC_PRIOR) {
+    result = "static prior";
+  } else if (mode == LEARNING_SAMPLING) {
+    result = "learning sampling";
+  } else if (mode == LEARNING_ALGORITHMC) {
+    result = "learning algorithm C";
+  } else if (mode == STATIC_MARGINALS) {
+    result = "static marginals";
+  } else if (mode == MAPPING) {
+    result = "mapping";
+  } else {
+    cout << "Invalid pick mode: " << mode << endl;
+    assert(0);
+  }
+  return result;
+}
