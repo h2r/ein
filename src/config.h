f23c3b98628fbfc3a4a7e44a524c0e7c7ac1f7e3
Stefanie Tellex <stefie10@cs.brown.edu>
2015-09-04 09:10:22 -0400

    new boundaries for ik
    Former-commit-id: 8e080de5189e81cfdad801ea2cb1cb724404e553

--- src/config.h
+++ src/config.h
@@ -1075,6 +1075,12 @@ class EinConfig {
   constexpr static double mapYMin = -1.5;
   constexpr static double mapYMax = 1.5;
   constexpr static double mapStep = 0.01;
+
+  //constexpr static double mapXMin = -0.75;
+  //constexpr static double mapXMax = 1.0; 
+  //constexpr static double mapYMin = -0.1;
+  //constexpr static double mapYMax = 1.25; 
+  //constexpr static double mapStep = 0.01; 
   
   double mapSearchFenceXMin;
   double mapSearchFenceXMax;
