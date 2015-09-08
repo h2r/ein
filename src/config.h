2c582e5c573be6d07cd8f5c8423d57f15a93643d
Stefanie Tellex <stefie10@cs.brown.edu>
2015-09-07 21:17:34 -0400

    fixing a bug in the map rendering; making the map the same dimensions as the wrist view, with new ik maps to match
    Former-commit-id: f93e3ae8bea17bf9c0bad45706acda8cfc062a94

--- src/config.h
+++ src/config.h
@@ -1087,11 +1087,19 @@ class EinConfig {
   vector<cv::Point> cBots;
 
 
-  constexpr static double mapXMin = -1.5;
-  constexpr static double mapXMax = 1.5;
+  constexpr static double mapXMin = -0.9375;
+  constexpr static double mapXMax = 0.9375;
   constexpr static double mapYMin = -1.5;
   constexpr static double mapYMax = 1.5;
   constexpr static double mapStep = 0.01;
+
+
+  //  constexpr static double mapXMin = -1.5;
+  //  constexpr static double mapXMax = 1.5; 
+  //  constexpr static double mapYMin = -1.5;
+  //  constexpr static double mapYMax = 1.5; 
+  //  constexpr static double mapStep = 0.01;
+
 
   //constexpr static double mapXMin = -0.75;
   //constexpr static double mapXMax = 1.0; 
