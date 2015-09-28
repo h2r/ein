9cb6f238deb79adc110beae24bf2279b18e661e3
Stefanie Tellex <stefie10@cs.brown.edu>
2015-09-28 12:52:08 -0400

    I think I got the capslock and modifier flags working
    Former-commit-id: 8d18419e8b34ffc5283f5a800992ca829465b442

--- src/qtgui/capturelineedit.cpp
+++ src/qtgui/capturelineedit.cpp
@@ -6,17 +6,22 @@ using namespace std;
 
 void CaptureLineEdit::keyPressEvent(QKeyEvent * event)
  {
-  QWidget::keyPressEvent(event);
+   QWidget::keyPressEvent(event);
 
    int key = event->key();
    
    Qt::Key qtkey = static_cast<Qt::Key>(key);
    char asciiCode = QTest::keyToAscii(qtkey);
-   if (asciiCode != 0)
+   if (asciiCode != 0) {
      key = static_cast<int>(asciiCode);
-   else
+   } else {
      key = event->nativeVirtualKey(); //same codes as returned by GTK-based backend
-   
+   }
+   key = event->nativeVirtualKey();
+
+   int modifiers = event->nativeModifiers();
+   key |= modifiers << 16;
+
    //control plus (Z, +, -, up, down, left, right) are used for zoom/panning functions
    if (event->modifiers() != Qt::ControlModifier)
     {
