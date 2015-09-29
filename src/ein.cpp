03f65f3a7d86202e637c4cd28ffcac148eb2affd
joberlin <john.gilmore.oberlin@gmail.com>
2015-09-28 22:14:04 -0400

    fixing whitespace in parsing problem, and fixing print

--- src/ein.cpp
+++ src/ein.cpp
@@ -2759,7 +2759,7 @@ void MachineState::evaluateProgram(const string program)  {
   for (unsigned int j = 0; j < tokens.size(); j++) {
     int i = tokens.size() - j - 1;
     trim(tokens[i]);
-    if (!ms->pushWord(tokens[i])) {
+    if (tokens[i] != "" && !ms->pushWord(tokens[i])) {
       cout << "Warning, ignoring unknown word from the forth topic: " << tokens[i] << endl;
     }
   }
