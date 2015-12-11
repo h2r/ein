97354780fc1f48a86d5e06a0927bff79485e4815
joberlin <john.gilmore.oberlin@gmail.com>
2015-12-11 00:29:34 -0500

    gaussian cell dot product
    Former-commit-id: a1783e40b6638da559eef41a1d3d50bc52a12491

--- src/gaussian_map.h
+++ src/gaussian_map.h
@@ -36,6 +36,7 @@ typedef struct _GaussianMapCell {
 
   void newObservation(Vec3b obs);
   
+  double innerProduct(_GaussianMapCell * other, double * rterm, double * gterm, double * bterm);
 } GaussianMapCell;
 
 class GaussianMap {
