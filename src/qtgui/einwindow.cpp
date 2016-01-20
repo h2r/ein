d7bea448d623c4a8e61e5749c169648f13943ec5
Stefanie Tellex <stefie10@cs.brown.edu>
2016-01-19 21:58:03 -0500

    menu to save images

--- src/qtgui/einwindow.cpp
+++ src/qtgui/einwindow.cpp
@@ -12,11 +12,38 @@ EinWindow::EinWindow(QWidget *parent, shared_ptr<MachineState> _ms) :
     ui->setupUi(this);
     ms = _ms;
     ui->imageFrame->layout()->addWidget(myView.getWidget());
+    cout << "menu: " << ui->menubar << endl;
+    cout << "menu: " << ui->menubar->isVisible() << endl;
+    ui->menubar->setVisible(true);
+    connect(ui->actionSaveImage, SIGNAL(triggered()), this, SLOT(saveImage()));
+}
+
+
+void EinWindow::saveImage() {
+  cout << "Saving image." << endl;
+  QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"),
+						  ".",
+						  tr("Images (*.png *.jpg)"));
+  cout << "filename: " << fileName.toStdString() << endl;
+  cout << "channels: " << myImage.channels() << endl;
+  cout << "type: " << myImage.type() << endl;
+  Mat dst;
+  if (myImage.channels() == 1) {
+    dst = myImage.clone();
+    dst = dst * 255;
+  } else {
+    dst = myImage;
+  }
+
+  cout << "dst channels: " << dst.channels() << endl;
+  
+  imwrite(fileName.toStdString(), dst);
 }
 
 void EinWindow::updateImage(const Mat image)  {
   if ( !isSketchyMat(image)) {
     myView.updateImage(image);
+    myImage = image;
   }
 }
 
