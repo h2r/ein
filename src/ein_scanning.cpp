72d1a01347f66f4ce4e5e0fc5d1af6772f5881d4
joberlin <john.gilmore.oberlin@gmail.com>
2016-04-28 01:01:08 -0400

    calibration added mostly tested

--- src/ein_scanning.cpp
+++ src/ein_scanning.cpp
@@ -2142,10 +2142,10 @@ END_WORD
 REGISTER_WORD(SetMagnificationA)
 
 CONFIG_GETTER_DOUBLE(CameraGetIdxMagX, ms->config.m_x_h[ms->config.currentThompsonHeightIdx]) 
-CONFIG_SETTER_DOUBLE(CameraSetIdxMaxX, ms->config.m_x_h[ms->config.currentThompsonHeightIdx]) 
+CONFIG_SETTER_DOUBLE(CameraSetIdxMagX, ms->config.m_x_h[ms->config.currentThompsonHeightIdx]) 
 
 CONFIG_GETTER_DOUBLE(CameraGetIdxMagY, ms->config.m_y_h[ms->config.currentThompsonHeightIdx]) 
-CONFIG_SETTER_DOUBLE(CameraSetIdxMaxY, ms->config.m_y_h[ms->config.currentThompsonHeightIdx]) 
+CONFIG_SETTER_DOUBLE(CameraSetIdxMagY, ms->config.m_y_h[ms->config.currentThompsonHeightIdx]) 
 
 WORD(SetMagnificationB)
 virtual void execute(std::shared_ptr<MachineState> ms) {
