bb7965fed473ec2b82174a4103762d76f3c1cffb
Stefanie Tellex <stefie10@cs.brown.edu>
2017-03-22 22:04:29 -0400

    test cases and stuff

--- src/ein_misc.cpp
+++ src/ein_misc.cpp
@@ -568,7 +568,7 @@ REGISTER_WORD(Rangle)
 
 WORD(Leq)
 virtual string description() {
-  return "Takes two words and returns 1 if they are less than or equal to (by value) and 0 otherwise."
+  return "Takes two words and returns 1 if they are less than or equal to (by value) and 0 otherwise.";
 }
 virtual vector<string> names() {
   vector<string> result;
