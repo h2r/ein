e8951de2f8bc787d3a3a90cfb55d6ecb90051ccc
Liyaan Maskati <liyaan_maskati@brown.edu>
2018-04-12 12:53:32 -0400

    bug fixes to make it work with Baxter

--- setup.sh
+++ setup.sh
@@ -15,3 +15,5 @@ export ROS_HOSTNAME=`hostname`
 export ROS_MASTER_URI=http://$ROBOT:11311
 export PS1="\[\033[00;33m\][jaco - ${ROS_MASTER_URI}]\[\033[00m\] $PS1"
 
+# If you are using baxter, make this file source baxter.sh
+# source baxter.sh
