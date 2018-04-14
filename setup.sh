38d6280956c28e2a937972cd08668d93f457c1fe
Stefanie Tellex <stefie10@cs.brown.edu>
2018-04-14 16:43:00 -0400

    updating screenrc stuff

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
-# This is an example from Stefanie's ethernet environment.  Copy to
-# your ws root directory and change the variables appropriately.  This
-# is supposed to be like ./baxter.sh.  It sets environment variables
-# for your environment.
-#
-# The pi.screenrc file requires this to be in the ws root.  You may
-# also set any other ROS environment variables here.
-if [ -n ${ROBOT} ]; then
+#!/bin/bash
+
+# This file, when sourced, sets your ROS environment variables.  It
+# should be placed in the root of the catkin workspace, since it also
+# sources the catkin setup.bash file.  It takes a ROBOT= enviornment
+# variable which it uses to set things.  You may need to edit this
+# file depending on your network configuration to set ROS_IP or
+# ROS_HOSTNAME for your machine. 
+
+if [ -z ${ROBOT} ]; then
 export ROBOT=localhost
 fi 
 
@@ -13,7 +15,7 @@ source devel/setup.bash
 #export ROS_IP=192.168.42.1
 export ROS_HOSTNAME=`hostname`
 export ROS_MASTER_URI=http://$ROBOT:11311
-export PS1="\[\033[00;33m\][jaco - ${ROS_MASTER_URI}]\[\033[00m\] $PS1"
+export PS1="\[\033[00;33m\][pidrone - ${ROS_MASTER_URI}]\[\033[00m\] $PS1"
 
 # If you are using baxter, make this file source baxter.sh and remove
 # the othe rstuff above, which is redundant if you are using
